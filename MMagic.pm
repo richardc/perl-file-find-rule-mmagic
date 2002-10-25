61b1e8a78aba3e5b97917c78fc245eea346110c6
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2002-10-25 15:38:16 +0000

    document and package
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/File-Find-Rule-MMagic@844 c56e198b-7db7-0310-b3ab-8e406f684e33

--- MMagic.pm
+++ MMagic.pm
@@ -20,3 +20,41 @@ sub File::Find::Rule::magic {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+File::Find::Rule::MMagic - rule to match on mime types
+
+=head1 SYNOPSIS
+
+ use File::Find::Rule::MMagic;
+ my @images = find( file => magic => 'image/*', in => '.' );
+
+=head1 DESCRIPTION
+
+File::Find::Rule::MMagic interfaces File::MMagic to File::Find::Rule
+enabling you to find files based upon their mime type.  Text::Glob
+is used so that the patter may be a simple globbing pattern.
+
+=head2 ->magic( @patterns )
+
+Match only things with the mime types specified by @patterns.  The
+specification can be a glob pattern, as provided by L<Text::Glob>.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>, from an idea by Mark Fowler.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2002 Richard Clamp.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<File::Find::Rule>, L<Text::Glob>, L<File::MMagic>
+
+=cut
